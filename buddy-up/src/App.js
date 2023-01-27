3f9a07529b10e7cd78aa1f0911e63ba061a77692
Sergio Colis <sergioacolis@ResNet-24-135.resnet.ucsb.edu>
2023-01-27 13:09:43 -0800

    SC - Test React Web App Template

---
+++ buddy-up/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World
+      </header>
+      <button className="button"> Test </button>
+    </div>
+  );
+}
+
+export default App;
