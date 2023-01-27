e9ce807c759e7933bdf7ff667b1997bdb276655d
Sergio Colis <sergioacolis@ResNet-24-135.resnet.ucsb.edu>
2023-01-27 13:48:31 -0800

    SC - Changed stuff and set up firebase + firestone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import handleSubmit from './handles/handlesubmit';
+import { useRef } from 'react';
 
 function App() {
+  const dataRef = useRef()
+
+  const submithandler = (e) => {
+    e.preventDefault()
+    handleSubmit(dataRef.current.value)
+    dataRef.current.value = ""
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Hello World
       </header>
-      <button className="button"> Test </button>
+      <form onSubmit={submithandler}>
+        <input type= "text" ref={dataRef} placeholder="Input is pushed to Firebase" />
+        <button type = "submit">Save</button>
+        </form>
     </div>
   );
 }
