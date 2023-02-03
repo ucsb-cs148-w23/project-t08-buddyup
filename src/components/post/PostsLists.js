3f09bfaa3132987f09bf704946e91fec5395281d
Anouki Panthagani <anouki@csil.cs.ucsb.edu>
2023-02-02 16:38:59 -0800

    ap- formatted message

--- src/components/post/PostsLists.js
+++ src/components/post/PostsLists.js
@@ -1,12 +1,17 @@
-import Post from "./index"
-import {Box} from "@chakra-ui/react";
+import Post from "./index";
+import { Box, Text } from "@chakra-ui/react";
+
 
 
 export default function PostsLists({posts}){
-    return <Box px="4">
-    {posts?.length ===0 
-    ? ("no posts") 
-    : posts?.map(post => <Post key={post.id} post={post}/>)}
+    return (
+    <Box px="4">
+    {posts?.length ===0 ? (
+    <Text textAlign="center" fontSize="xl">No posts yet...Make the first one and find a roommate!</Text>
+    ) : (
+        posts?.map((post) => <Post key={post.id} post={post}/>)
+    )}
 
     </Box>
+);
 }
