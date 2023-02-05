2c5af337f0e40820176595a864afad46a5229afb
Lauren Daniel <laurendaniel@ucsb.edu>
2023-02-04 17:38:34 -0800

    post list changes

--- src/components/post/PostsLists.js
+++ src/components/post/PostsLists.js
@@ -5,9 +5,10 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function PostsLists({posts}){
     return (
-    <Box px="4">
+    <Box px="4" align="center">
     {posts?.length ===0 ? (
-    <Text textAlign="center" fontSize="xl">No posts yet...Make the first one and find a roommate!</Text>
+    <Text textAlign="center" fontSize="xl">
+        No posts yet...Make the first one and find a roommate!</Text>
     ) : (
         posts?.map((post) => <Post key={post.id} post={post}/>)
     )}
