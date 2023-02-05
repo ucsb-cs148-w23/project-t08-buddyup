1d26d8d90cf6c62efd80649d492ead9f75fbc971
Sergio Colis <sergioacolis@Sergios-MacBook-Pro.local>
2023-02-04 16:11:48 -0800

    SC - Put in Old Login

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,8 +1,14 @@
 import Profile from 'components/profile';
+import Login from 'login';
+import Mainpage from 'main';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const PROFILE = "/profile";
+export const LOGIN = "/";
+export const MAINPAGE = "/main";
 
 export const router = createBrowserRouter ([
     { path: PROFILE, element: <Profile /> },
+    { path: LOGIN, element: <Login /> },
+    { path: MAINPAGE, element: <Mainpage /> },
 ]);
