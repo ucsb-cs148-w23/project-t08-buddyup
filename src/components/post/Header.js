23eb9c07387c20714e938cfead9512b24dd06d3b
Kaiwen <kaiwentang@umail.ucsb.edu>
2023-02-06 19:43:00 -0800

    attempt to add header for posts

--- src/components/post/Header.js
+++ src/components/post/Header.js
@@ -1,11 +1,11 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 //import Avatar from "components/profile/Avatar";
-import { useUser } from "hooks/users";
-import { formatDistanceToNow } from "date-fns";
-import UsernameButton from "components/profile/UsernameButton";
+//import { useUser } from "hooks/users";
+//import { formatDistanceToNow } from "date-fns";
+//import UsernameButton from "components/profile/UsernameButton";
 
-export default function Header({ post }) {
-  const { date } = post;//deleted uid
+export default function Header() {
+  //const { date } = post;//deleted uid
   //const { user, isLoading } = useUser(uid);
 
   //if (isLoading) return "Loading...";
@@ -18,14 +18,7 @@ export default function Header({ post }) {
       p="3"
       bg="gray.50"
     >
-      {/*<Avatar user={user} size="md" />*/}
-
-      <Box ml="4">
-        <UsernameButton user={user} />
-        <Text fontSize="sm" color="gray.500">
-          {formatDistanceToNow(date)} ago
-        </Text>
-      </Box>
+      
     </Flex>
   );
 }
