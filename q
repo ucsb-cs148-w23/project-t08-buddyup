4b45c4bd9ea73c58ed1711ca3b88425d0aaa11b4
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2023-02-08 13:50:06 -0800

    Posts show up on dashboard now

--- q
+++
@@ -1,9 +0,0 @@
-  ProfileV2[m
-* [32mProfileV3[m
-  kai_helloworld[m
-  khCreatePostPage[m
-  khProfileHookStuff[m
-  khTestBranch[m
-  ktloginButton[m
-  main[m
-  sckhProfile[m
--- src/hooks/posts.js
+++ src/hooks/posts.js
@@ -20,50 +20,15 @@ export function useAddPost() {
         //setLoading(true);
         const id = uuidv4();
         const uid = auth.currentUser.uid;
+        const name = auth.currentUser.displayName;
         console.log("UID:");
         console.log(uid);
-        await setDoc(doc(firestore, "postsTest", id, uid), {
-            ...post, 
-            id,
-            date: Date.now(),
-            uid,
-        })
-    }
-    return {addPost};
-}
-
-export function usePosts(uid = null) {
-
-    
-    const q = uid
-        ? query(collection(firestore, "postsTest"), orderBy("date", "desc"), where("uid","==",uid))
-        : query(collection(firestore, "postsTest"), orderBy("date", "desc"));
-    const [posts,isLoading, error] = useCollectionData(q);
-    
-    if (error) throw error;
-    return {posts, isLoading}; 
-}
-
-export function usePost(id) {
-    const q = doc(firestore, "postsTest", id);
-    const [post, isLoading] = useDocumentData(q);
-  
-    return { post, isLoading };
-  }
-
-
-/*export function useAddPost() {
-    //const [isLoading, setLoading] = useState(false);
-    async function addPost(post) {
-        //setLoading(true);
-        const id = uuidv4();
-        // const uid = auth.currentUser.uid;
-        // await setDoc(doc(firestore, "posts", id, uid), {
         await setDoc(doc(firestore, "posts", id), {
             ...post, 
             id,
             date: Date.now(),
-            // uid,
+            uid,
+            name,
         })
     }
     return {addPost};
@@ -87,4 +52,3 @@ export function usePost(id) {
   
     return { post, isLoading };
   }
-  */
