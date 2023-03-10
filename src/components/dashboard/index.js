bfe1ae9aac6b859845625cbfea57d1dc60ada13e
Kai Hilbourne <kaihilbourne@umail.ucsb.edu>
2023-03-10 13:44:33 -0800

    KH - initial filter attempt

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -8,6 +8,42 @@ import { useGoToProfile } from 'hooks/users';
 import { auth } from 'firebase_setup/firebase';
 import { CustomCheckbox } from './CheckBox';
 
+function FilterPosts() {
+    const { handleSubmit } = useForm();
+    const { value, getCheckboxProps } = useCheckboxGroup();
+    const { value:value2, getCheckboxProps:getCheckboxProps2 } = useCheckboxGroup();
+
+    return <Box maxW="600px" mx="auto" py="10">
+    <form onSubmit={handleSubmit(console.log("filtering ... jk"))}>
+        <HStack justify="space-between">
+            <Heading>
+                Filter Posts
+            </Heading>
+            <Button 
+            type="submit"
+            >
+                Filter
+            </Button>
+        </HStack>
+        <Stack>
+            <Text>Find posts looking for {value.length > 0 ? value.sort().join(' and ') : "nothing"}
+            {value2.length > 0 ? " in " + value2.join(' and ') : ""}</Text>
+            <Flex>
+                <CustomCheckbox {...getCheckboxProps({ value: 'Housemates' })}/>
+                <CustomCheckbox {...getCheckboxProps({ value: 'Housing' })}/>
+            </Flex>
+            <Flex>
+                <CustomCheckbox {...getCheckboxProps2({ value: 'Isla Vista' })}/>
+                <CustomCheckbox {...getCheckboxProps2({ value: 'University Housing' })}/>
+                <CustomCheckbox {...getCheckboxProps2({ value: 'Goleta' })}/>
+            </Flex>
+        </Stack>
+       
+    </form>
+    
+</Box>
+}
+
 function NewPost() {
     const {register, handleSubmit, reset} = useForm();
     const {addPost} = useAddPost();
@@ -40,8 +76,10 @@ function NewPost() {
         <Textarea resize="none" 
         mt="5" 
         placeholder="Title your post"
-        height={"30px"}
+        minHeight={"40px"}
+        height={"40px"}
         minRows={1}
+        maxLength={"120"}
         {...register("title")}
         />
         <Stack>
@@ -88,6 +126,12 @@ export default function Dashboard() {
     }
 
     const{posts, isLoading} = usePosts();
+    const posting = true;
+
+    async function handleToggle() {
+        posting = !posting;
+    }
+
     return (
     <>
         <HStack spacing={"10"}>
@@ -96,13 +140,22 @@ export default function Dashboard() {
                     Sign Out
                 </Button>
             </form>
+            <form onSubmit={handleSubmit(handleToggle)}>
+                <Button type="submit" >
+                    {posting
+                    ? "Filter Posts"
+                    : "Make a New Post"}
+                </Button>
+            </form>
             <form onSubmit = {handleSubmit(handleProfile)}>
                 <Button type="submit">
                     Profile
                 </Button>
             </form>
         </HStack>
-        <NewPost />
+        {posting
+        ? <NewPost />
+        : <FilterPosts />}
         {isLoading
         ? <Text>Posts are loading ...</Text>
         : <PostsLists posts={posts}/>}
